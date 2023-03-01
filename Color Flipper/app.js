1390c77d499fc4b7009745733dff54af36462735
Vitalii <vitalyorishko@gmail.com>
2023-03-01 18:54:30 +0200

    first pr 'Color Flippe'

---
+++ Color Flipper/app.js
@@ -0,0 +1,15 @@
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025', 'purple', 'pink', 'grey', 'yellow'];
+
+const button = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+
+button.addEventListener('click', () => {
+    const random = randomNumber();
+    document.body.style.backgroundColor = colors[random];
+    color.textContent = colors[random];
+});
+
+function randomNumber () {
+    return Math.floor(Math.random() * colors.length);
+}
---
+++ Color Flipper/hex.js
@@ -0,0 +1,17 @@
+const colorsHex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const button = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+    let hex = "#";
+    for (let i = 0; i < 6; i++) {
+        hex += colorsHex[randomNumber()];  
+    }
+    document.body.style.backgroundColor = hex;
+    color.textContent = hex;
+})
+
+function randomNumber () {
+    return Math.floor(Math.random() * colorsHex.length);
+}
